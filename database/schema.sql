74d458bb36ace0cc87de9c938c938cb716bbbb1c
Klara <klarasan@kth.se>
2022-01-28 12:25:54 +0100

    added schemas

---
+++ database/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS Event CASCADE;
+DROP TABLE IF EXISTS User CASCADE;
+DROP TABLE IF EXISTS Purchase CASCADE;
+DROP TABLE IF EXISTS Ticket CASCADE;
+
+CREATE TABLE Event (
+    EventID SERIAL PRIMARY KEY,
+    Name varchar(255) NOT NULL,
+    Price double precision NOT NULL,
+    StartTime timestamp NOT NULL,
+    EndTime timestamp,
+    Location varchar(255) NOT NULL,
+    NumTick int NOT NULL
+);
+
+CREATE TABLE User (
+    UserID SERIAL PRIMARY KEY,
+    Mail varchar(255) NOT NULL
+);
+
+CREATE TABLE Purchase (
+    PurchaseID SERIAL PRIMARY KEY,
+    EventID int REFERENCES Event(EventID) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
+    UserID int REFERENCES User(UserID) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
+    PurchaseTime timestamp NOT NULL,
+    PaymentMethod varchar(255)
+);
+
+CREATE TABLE Ticket (
+    TicketID SERIAL PRIMARY KEY,
+    PurchaseID int REFERENCES Purchase(PurchaseID) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE,
+    UserID int REFERENCES User(UserID) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE
+);
