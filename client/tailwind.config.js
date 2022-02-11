e95fa116abf32d9e374768c6d64459d67503492f
Adrian Salamon <adr.salamon@gmail.com>
2022-02-11 22:36:44 +0100

    Removed database files and homogonized tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
