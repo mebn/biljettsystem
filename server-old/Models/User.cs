72f4a05918ae67e530222d0fdf67a7e7d92977ff
Marcus Nilszén <mebn122@gmail.com>
2022-02-21 18:49:09 +0100

    removed old c# server

--- server-old/Models/User.cs
+++
@@ -1,7 +0,0 @@
-namespace server.Models {
-    public class User {
-        public int UserId { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-    }
-}
