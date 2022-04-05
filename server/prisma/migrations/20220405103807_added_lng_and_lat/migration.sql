ebc9abd0f45c2751073676e69428bfa3aba5073b
Adrian Salamon <adr.salamon@gmail.com>
2022-04-05 15:50:48 +0200

    Fixed a migration

--- server/prisma/migrations/20220405103807_added_lng_and_lat/migration.sql
+++ server/prisma/migrations/20220405103807_added_lng_and_lat/migration.sql
@@ -1,14 +1,6 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `x` on the `eventLocation` table. All the data in the column will be lost.
-  - You are about to drop the column `y` on the `eventLocation` table. All the data in the column will be lost.
-  - Added the required column `lat` to the `eventLocation` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `lng` to the `eventLocation` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "eventLocation" DROP COLUMN "x",
-DROP COLUMN "y",
-ADD COLUMN     "lat" DOUBLE PRECISION NOT NULL,
-ADD COLUMN     "lng" DOUBLE PRECISION NOT NULL;
+ALTER TABLE "eventLocation" 
+RENAME COLUMN "x" TO "lat";
+
+ALTER TABLE "eventLocation" 
+RENAME COLUMN "y" TO "lng";
