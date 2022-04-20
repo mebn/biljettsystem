d0d9a6cd20f33cf50834a4f7e9dd3bb823f5717d
root <root@Harry-PC.localdomain>
2022-04-20 14:59:29 +0200

    Change popup to look more similar to others

--- client/src/components/LoginPopup/LoginPopup.jsx
+++ client/src/components/LoginPopup/LoginPopup.jsx
@@ -15,14 +15,14 @@ const googleLogin = () =>{
 const LoginPopup = (props) => {
     return (
         <div className="popup-box">
-            <div className="fixed inset-0 bg-black bg-opacity-30 z-[1050]" />
+            <div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity z-[1050]" />
             <div className="fixed inset-0 z-[1100] md:max-w-sm md:mr-auto md:ml-auto md:inset-56 md:shadow-lg">
-                <div className="h-full z-50 overflow-auto md:rounded-xl bg-white">
+                <div className="h-full z-50 overflow-auto md:rounded-lg bg-[#f5f5f5]">
                     <div className="font-bold pt-8 pb-8 border-b text-2xl text-center">Logga in</div>
                     <div className="flex flex-col justify-center items-center">
                         
                         <button
-                            className="bg-[#eeeeee] hover:bg-[#bbbbbb] shadow-sm w-72 h-16 rounded-lg 
+                            className="bg-[#e9e9e9] hover:bg-[#bbbbbb] shadow-sm w-72 h-16 rounded-lg 
                             flex justify-center items-center transition duration-200 ease-in-out mt-32 md:mt-16" 
                             onClick={googleLogin}
                         >
@@ -32,11 +32,8 @@ const LoginPopup = (props) => {
                         </button>
                     </div>
 
-                    <div
-                        className="fixed cursor-pointer hover:bg-[#ddd] ease-in-out md:absolute top-5 bg-gray-50  rounded-full p-2 shadow-md right-5 z-[100]"
-                        onClick={props.handleClose}
-                    >
-                        <XIcon className="h-7 w-7" />
+                    <div className='fixed cursor-pointer hover:bg-[#ddd] transition ease-in-out duration-200 md:absolute top-20 md:top-5 bg-[#f5f5f5] md:bg-transparent rounded-full p-2 shadow-md md:shadow-none right-5 z-[100]' onClick={props.handleClose}>
+                    <XIcon className="h-7 w-7"/>
                     </div>
                 </div>
             </div>
