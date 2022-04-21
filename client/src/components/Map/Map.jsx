7487048e871032b96da967c6fd5a09cddb8b3bdf
Adrian Salamon <adr.salamon@gmail.com>
2022-04-21 09:09:57 +0200

    Added loading animation to event page

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,4 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/images/marker-shadow.png";
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+let Icon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
 
 const Map = (props) => {
   const center = {lat: props.location.lat, lng: props.location.lng}
@@ -12,7 +23,7 @@ const Map = (props) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={center}>
+      <Marker position={center} icon={Icon}>
         <Popup>{props.location.address}</Popup>
       </Marker>{" "}
     </MapContainer>
